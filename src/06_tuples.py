79d9caacbfa8912af945b0c49a50c55e3639fc08
Jackson McComas <jackson.mccomas@gmail.com>
2020-03-10 11:14:15 -0500

    6

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -17,12 +17,14 @@ automatically.
 
 import math
 
+
 def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
     x1, y1 = b
 
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+
 
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
@@ -31,14 +33,25 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
+def print_tuple(atuple):
+    if len(atuple) == 1:
+        print(atuple[0])
+    else:
+        a, b, c, d, e = atuple
+        print(a)
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+
 
 # YOUR CODE HERE
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
